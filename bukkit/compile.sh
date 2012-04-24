4b9069c7d79631f0c454ea6088128437790b0c50
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-04-23 20:40:39 -0700

    Move up

---
+++ bukkit/compile.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+CLASSPATH=../../craftbukkit-1.2.5-R1.1-MCPC-SNAPSHOT-71.jar javac *.java
--- compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-CLASSPATH=../craftbukkit-1.2.5-R1.1-MCPC-SNAPSHOT-71.jar javac bukkit/*.java
